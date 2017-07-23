dbd7d70177277286fe322f224b87132b87c4910a
myvary <727484328@qq.com>
2017-07-23 19:10:32 +0800

    移除页面解析函数

--- main/main.rb
+++ main/main.rb
@@ -1,9 +1,9 @@
 #encoding = utf-8
 require '../support'
+require '../parse/parse'
 require 'json'
 require 'pp'
-class Parse
-  public
+class Main
   def initialize
     @dow = Dow.new
   end
@@ -18,6 +18,14 @@ class Parse
 
   def parse_html(url,key)
     html = dow_html(url)
+    #如果返回为"0"，休眠4秒再次发起请求
+    if html == "0"
+      puts "第一次尝试失败，休眠4秒后重新开始！"
+      sleep(4)
+      puts "开始！"
+      html = dow_html(url)
+    end
+
     if login_yes?(html)
       puts "天眼查开始限制查询！需要登录！"
     else
@@ -28,7 +36,11 @@ class Parse
         if key == txt
           url_details  = div['href']
           puts "#{key}-->天眼查页面#{url_details}"
-          parse_details(url_details)
+          #天眼查企业详情页
+          html_info = @dow.download_html(url_details)
+          #解析页面
+          basic_info(html_info)
+
           break
         else
           puts "没有找到！"
@@ -37,23 +49,24 @@ class Parse
     end
   end
 
-  def parse_details(url_details)
-    html_info = @dow.download_html(url_details)
-    html_info.xpath('//div[@class="c8"]').each do |td|
-      puts td.content.gsub(" ","").gsub("\n","")
-    end
-  end
-
-  private
+  
   def dow_html(url)
     @dow.download_html(url)
   end
 end
 
-pas = Parse.new
+pas = Main.new
+parse = Parse.new
+file = pas.dow_html("file:///Users/liuxin/RubymineProjects/tianyancha/main/test.html")
+
 root = 'https://www.tianyancha.com/search?key='
 pm = '&checkFrom=searchBox'
 key = "爱尔眼科医院集团股份有限公司"
 url = root + key + pm
-pas.parse_html(url,key)
+parse.contact(file)#股票代码和联系方式
+parse.parse_admin(file)#解析主要人员
+parse.parse_intro(file)#解析企业简介
+parse.parse_shareholder(file)#解析股东信息
+parse.parse_basic_info(file)#解析企业基本信息，所有企业都有
+# pas.parse_html(url,key)
 
