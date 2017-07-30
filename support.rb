97bd680b5c51363e380f41de4ca3b4bc0a75e5f3
myvary <727484328@qq.com>
2017-07-30 21:03:33 +0800

    删掉注释

--- support.rb
+++ support.rb
@@ -6,35 +6,16 @@ require 'open-uri'
 class Dow
   def download_html(url)
     puts "开始下载#{url}页面！"
-    # dali = Get_xici_daili.new
-    # ip_list = Array.new
     begin
       agent = Mechanize.new
-      # puts "当前代理池，存在#{ip_list.size}个！"
-      # while true
-      #   if ip_list.size==0
-      #     ip_list = dali.parse_ip
-      #     break
-      #   else
-      #     break
-      #   end
-      # end
-      # random = Random.new(ip_list.size)
-      # #随机取出一个代理
-      # ip_post = ip_list[random]
-      # ip = ip_post[0,ip_post.index(":")]
-      # post = ip_post[ip_post.index(":"),ip_post.size]
-      agent.set_proxy "111.126.94.80",808
-      # 设置超时
-      # agent.open_timeout = 4
+      # agent.set_proxy "111.126.94.80",808
       agent.user_agent_alias = 'Mac Safari'
-      
       html = agent.get(url)
       return html
     rescue Exception=> e
       puts "出现异常:~~"+e.message+"~~#{url}该链接无法抓取"
       # ip_list.delete(ip_post)
-      return "0"
+      return false
     end
   end  
 end
