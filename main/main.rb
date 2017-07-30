7fd836a089604222a2d30f1f56d3f229c3494818
myvary <727484328@qq.com>
2017-07-30 21:04:22 +0800

    调整代码

--- main/main.rb
+++ main/main.rb
@@ -16,6 +16,27 @@ class Main
     flog == 1? true:false 
   end
 
+  def seek_title?(html,key)
+    html.xpath('//div[@class="search_right_item"]').each do |div|
+      ##历史名称
+      flog = 0
+      div.xpath('div/div/div/div[@class="add"]').each do |div|
+        txt = div.content.gsub(/\s|\n/,"")
+        if  txt =~/历史名称:(.*)/
+          flog = 1
+        end
+      end
+      div.xpath('div/div/a').each do |div|
+        txt = div.content.gsub(/\n/,"")
+        if txt ==key || flog
+        puts div['href']
+        return 
+      end
+      end
+    end
+  end
+
+
   def parse_html(url,key)
     html = dow_html(url)
     #如果返回为"0"，休眠4秒再次发起请求
@@ -40,7 +61,6 @@ class Main
           html_info = @dow.download_html(url_details)
           #解析页面
           basic_info(html_info)
-
           break
         else
           puts "没有找到！"
@@ -54,21 +74,22 @@ class Main
     @dow.download_html(url)
   end
 end
-  def proxy_list
-    file = File.open('proxy.csv','r')
-    file.each_line do |proxy|
-      puts rand(4).to_s,proxy.split(':')
-    end 
-  end
+def proxy_list
+  file = File.open('proxy.csv','r')
+  file.each_line do |proxy|
+    puts rand(4).to_s,proxy.split(':')
+  end 
+end
 # proxy_list
 pas = Main.new
-# parse = Parse.new
-# file = pas.dow_html("file:///Users/liuxin/RubymineProjects/tianyancha/main/test3.html")
+parse = Parse.new
+file = pas.dow_html("file:///Users/liuxin/RubymineProjects/tianyancha/main/test6.html")
 # proxy_list
 root = 'https://www.tianyancha.com/search?key='
 pm = '&checkFrom=searchBox'
 key = "爱尔眼科医院集团股份有限公司"
 url = root + key + pm
+pas.seek_title?(file,"贵阳白云城市建设投资有限公司")
 # # parse.contact(file)#股票代码和联系方式
 # # parse.parse_admin(file)#解析主要人员
 # parse.parse_intro(file)#解析企业简介
@@ -77,5 +98,5 @@ url = root + key + pm
 # parse.parse_basic_info(file)#解析企业基本信息，所有企业都有
 # parse.parse_publish(file)#发行相关
 # parse.parse_financing(file)#融资历史
-puts pas.dow_html(url).body
+# puts pas.dow_html(url).body
 
